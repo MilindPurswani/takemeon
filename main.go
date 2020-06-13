c119d1457962658b71dcbe02a27e901d056445dd
MilindPurswani <milindpurswani@gmail.com>
2020-06-13 17:00:55 +0530

    Added Concurrency

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"net"
 	"os"
+	"sync"
 
 	"github.com/miekg/dns"
 )
@@ -13,10 +15,9 @@ type domainList []string
 
 func main() {
 
-	// TODO: Implement Concurrency
-	// var concurrency int
-	// flag.IntVar(&concurrency, "c", 20, "set the concurrency level")
-	// flag.Parse()
+	var concurrency int
+	flag.IntVar(&concurrency, "c", 20, "set the concurrency level")
+	flag.Parse()
 	config, _ := dns.ClientConfigFromFile("/etc/resolv.conf")
 	c := new(dns.Client)
 	m := new(dns.Msg)
@@ -28,27 +29,36 @@ func main() {
 	if err := sc.Err(); err != nil {
 		fmt.Fprintf(os.Stderr, "failed to read input from file %s\n", err)
 	}
-	for _, d := range domains {
-		_, err := net.LookupCNAME(d)
-		if err != nil {
-			m.SetQuestion(dns.Fqdn(d), dns.TypeCNAME)
-			m.RecursionDesired = true
-			r, _, err2 := c.Exchange(m, config.Servers[0]+":"+config.Port)
-			// Check if the domain is actually not existing
-			if err2 != nil {
-				continue
-			}
-			// Check to see if the Answer's length is 0
-			if len(r.Answer) == 0 {
-				continue
-			}
-			if r, ok := r.Answer[0].(*dns.CNAME); ok {
-				fmt.Print(d + " | ")
-				fmt.Println(r.Target[:len(r.Target)-1])
+
+	wg := new(sync.WaitGroup)
+	for i := 0; i < concurrency; i++ {
+		wg.Add(1)
+		go func() {
+
+			for _, d := range domains {
+				_, err := net.LookupCNAME(d)
+				if err != nil {
+					m.SetQuestion(dns.Fqdn(d), dns.TypeCNAME)
+					m.RecursionDesired = true
+					r, _, err2 := c.Exchange(m, config.Servers[0]+":"+config.Port)
+					// Check if the domain is actually not existing
+					if err2 != nil {
+						continue
+					}
+					// Check to see if the Answer's length is 0
+					if len(r.Answer) == 0 {
+						continue
+					}
+					if r, ok := r.Answer[0].(*dns.CNAME); ok {
+						fmt.Print(d + " | ")
+						fmt.Println(r.Target[:len(r.Target)-1])
+					}
+
+				}
 			}
 
-		}
-
+			wg.Done()
+		}()
 	}
-
+	wg.Wait()
 }
